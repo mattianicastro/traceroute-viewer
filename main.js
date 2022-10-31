ef2386e48009abcf425499eece790b3032d44d88
mattianicastro <nicastromattia@outlook.com>
2022-10-31 19:24:38 +0100

    fix async mess

--- main.js
+++ main.js
@@ -12,81 +12,91 @@ const mapElement = document.getElementById("map")
 autoAnimate(ipListElement)
 const ipList = []
 const startIcon = L.icon({
-  iconUrl: 'flag-start.svg', iconSize: [25, 41],
-  iconAnchor: [0, 35],
-  popupAnchor: [10, -34],
-  tooltipAnchor: [16, -28]
+    iconUrl: 'flag-start.svg', iconSize: [25, 41],
+    iconAnchor: [0, 35],
+    popupAnchor: [10, -34],
+    tooltipAnchor: [16, -28]
 });
 
 const endIcon = L.icon({
-  iconUrl: 'flag-end.svg', iconSize: [25, 41],
-  iconAnchor: [0, 35],
-  popupAnchor: [10, -34],
-  tooltipAnchor: [16, -28]
+    iconUrl: 'flag-end.svg', iconSize: [25, 41],
+    iconAnchor: [0, 35],
+    popupAnchor: [10, -34],
+    tooltipAnchor: [16, -28]
 });
 
 
-const showError = (message) => {
-  alert(message)
+const showToast = (message) => {
+    console.log(message)
 }
 
 const addIp = (ip) => {
-  ipList.push(ip)
-  const liElement = document.createElement("li")
-  liElement.innerText = ip
-  ipListElement.appendChild(liElement)
+    ipList.push(ip)
+    const liElement = document.createElement("li")
+    liElement.innerText = ip
+    ipListElement.appendChild(liElement)
 }
 
 ipInputBtn.addEventListener("click", (e) => {
-  const ipValue = ipInputElement.value
-  if (!ipValue) {
-    showError("Please enter an IP address")
-    return
-  }
-  if (!isV4Format(ipValue) && !isV6Format(ipValue)) {
-    showError("Invalid IP address")
-    return
-  }
-  if (isPrivate(ipValue)) {
-    showError("Private IP address")
-    return
-  }
-  ipInputElement.value = ""
-  addIp(ipValue)
+    const ipValue = ipInputElement.value
+    if (!ipValue) {
+        showToast("Please enter an IP address")
+        return
+    }
+    if (!isV4Format(ipValue) && !isV6Format(ipValue)) {
+        showToast("Invalid IP address")
+        return
+    }
+    if (isPrivate(ipValue)) {
+        showToast("Private IP address")
+        return
+    }
+    ipInputElement.value = ""
+    addIp(ipValue)
 })
 
 document.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    ipInputBtn.click()
-  }
+    if (e.key === "Enter") {
+        ipInputBtn.click()
+    }
 })
 
 const fetchIpLocation = async (ip) => {
-  const res = await fetch(`https://ipapi.co/${ip}/json/`)
-  const data = await res.json()
-  return data
+    const res = await fetch(`https://ipapi.co/${ip}/json/`)
+    const data = await res.json()
+    return data
+}
+
+const fetchAllIpLocations = async (ipList) => {
+    const locations = await Promise.allSettled(ipList.map(fetchIpLocation))
+    return locations.filter((location) => location.status === "fulfilled").map((location) => location.value)
 }
 
 renderMapBtn.addEventListener("click", (e) => {
-  appElement.style.display = "none"
-  mapElement.style.display = "unset"
-  const coords = []
-  var map = L.map('map').setView([0, 0], 2);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+    showToast("Fetching IP locations...")
+    fetchAllIpLocations(ipList).then(locations => {
+        console.log(locations)
+        appElement.style.display = "none"
+        mapElement.style.display = "unset"
+        const coords = []
+        var map = L.map('map').setView([0, 0], 2);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        var polyline = L.polyline(coords, { color: 'red' }).addTo(map);
+        locations.forEach((data, i)=>{
+            if(data.error){
+                console.log("skipped")
+                return
+            }
+            const icon = i === 0 ? startIcon : i === ipList.length - 1 ? endIcon : new L.Icon.Default()
+            var marker = L.marker([data.latitude, data.longitude], { icon: icon }).addTo(map)
+            marker.bindPopup(`<b>${data.ip}</b><br>${data.city}, ${data.region} (${data.country})<br>${data.asn} | ${data.org}`)
+            coords.push([data.latitude, data.longitude])
+            polyline.setLatLngs(coords)
+            polyline.redraw() 
+        })
 
-  var polyline = L.polyline(coords, { color: 'red' }).addTo(map);
-  ipList.forEach((ip, i) => {
-    const icon = i === 0 ? startIcon : i === ipList.length - 1 ? endIcon : new L.Icon.Default()
-    fetchIpLocation(ip).then((data) => {
-      var marker = L.marker([data.latitude, data.longitude], { icon: icon }).addTo(map)
-      marker.bindPopup(`<b>${data.ip}</b><br>${data.city}, ${data.region} (${data.country})<br>${data.asn} | ${data.org}`)
-      coords.push([data.latitude, data.longitude])
-      polyline.setLatLngs(coords)
-      polyline.redraw()
+
     })
-  })
-  console.log(coords)
-
 })
